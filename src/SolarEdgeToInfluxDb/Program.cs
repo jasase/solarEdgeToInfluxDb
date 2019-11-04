961bbda04fa0c392dd1ac28009b455dbea1cc15f
Sternheim, Sascha <sascha.sternheim@conti.de>
2019-11-04 14:08:20 +0100

    Project created

---
+++ src/SolarEdgeToInfluxDb/Program.cs
@@ -0,0 +1,10 @@
+﻿using ServiceHost.Docker;
+
+namespace SolarEdgeToInfluxDb
+{
+    public class Program : Startup
+    {
+        static void Main(string[] args)
+            => new Program().Run(args);
+    }
+}
