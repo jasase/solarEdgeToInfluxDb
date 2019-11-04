ab39e672ed13e05d3bc5ca33abfc6256500573a6
Sternheim, Sascha <sascha.sternheim@conti.de>
2019-11-04 14:25:41 +0100

    Job Stubs

---
+++ src/SolarEdgeToInfluxDb/SolarEdgeHistoryJob.cs
@@ -0,0 +1,17 @@
+﻿using Framework.Abstraction.Services.Scheduling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarEdgeToInfluxDb
+{
+    public class SolarEdgeHistoryJob : IJob
+    {
+        public string Name => "SolarEdge-History";
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SolarEdgeToInfluxDb/SolaredgePowerFlowJob.cs
@@ -0,0 +1,17 @@
+﻿using Framework.Abstraction.Services.Scheduling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarEdgeToInfluxDb
+{
+    public class SolaredgePowerFlowJob : IJob
+    {
+        public string Name => "SolarEdge-PowerFlow";
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
