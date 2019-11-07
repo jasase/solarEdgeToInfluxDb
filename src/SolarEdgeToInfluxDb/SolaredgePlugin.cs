012813eb7d9ea27fd7cb06644245e48ef78c4b86
Sascha Sternheim <sascha.sternheim@gmail.com>
2019-11-07 10:37:23 +0100

    Changed PowerFlow request time

--- src/SolarEdgeToInfluxDb/SolaredgePlugin.cs
+++ src/SolarEdgeToInfluxDb/SolaredgePlugin.cs
@@ -42,7 +42,7 @@ namespace SolarEdgeToInfluxDb
 
             var scheduler = Resolver.GetInstance<ISchedulingService>();
             scheduler.AddJob(historyJob, new PollingPlan(TimeSpan.FromHours(1.5)));
-            scheduler.AddJob(powerFlowJob, new PollingPlan(TimeSpan.FromSeconds(30)));
+            scheduler.AddJob(powerFlowJob, new PollingPlan(TimeSpan.FromSeconds(15)));
         }
     }
 }
